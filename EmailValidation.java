5c7443bf9cc1e3b9becaaf3962fb8cb36d860455
rksgr <r_kjha@yahoo.com>
2021-12-30 10:23:38 +0530

    Welcome Message

---
+++ EmailValidation.java
@@ -0,0 +1,6 @@
+public class EmailValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+        System.out.println("Today we shall perform email ID validation.");
+    }
+}
